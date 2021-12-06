bbdad04a125ffe100d71ccd64188add80b335502
nmart510 <nicholas.pa.ma@gmail.com>
2021-12-05 23:17:42 -0800

    turning

---
+++ 451 Final/Assets/Source/Model/BoatMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatMovement : MonoBehaviour
+{
+    float turnAngle = 0;
+    float velocity = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (turnAngle <= 20 && turnAngle >= -20)
+            turnAngle += Input.GetAxis("Horizontal");
+        else if (turnAngle > 20 )
+            turnAngle = 20;
+        else
+            turnAngle = -20;
+        if (velocity <= .3f && velocity >= -.15f)
+            velocity += .01f * Input.GetAxis("Vertical");
+        else if (velocity > .3f)
+            velocity = .3f;
+        else 
+            velocity = -.15f;
+        Vector3 rot = transform.eulerAngles;
+        transform.rotation = Quaternion.Euler(rot.x,rot.y+turnAngle*Time.deltaTime,rot.z);
+        transform.GetChild(0).localRotation = Quaternion.Euler(0,-turnAngle,0);
+        transform.GetChild(1).localRotation = Quaternion.Euler(0,turnAngle,0);
+    }
+}
