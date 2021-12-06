be949e600589e3b913d08d0ff89aa29a15400fe3
Bill Pham <47372471+billphaml@users.noreply.github.com>
2021-12-06 03:09:44 -0800

    Added local check

--- 451 Final/Assets/Source/Model/BoatMovement.cs
+++ 451 Final/Assets/Source/Model/BoatMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BoatMovement : MonoBehaviour
 {
+    public GameObject boat;
+
     float turnAngle = 0;
     float velocity = 0;
     public GameObject piston;
@@ -11,6 +13,7 @@ public class BoatMovement : MonoBehaviour
     float force = 0;
     bool reloaded = true; 
     bool firing = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,40 +23,52 @@ public class BoatMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (turnAngle <= 20 && turnAngle >= -20)
-            turnAngle += Input.GetAxis("Horizontal")*.1f;
-        else if (turnAngle > 20 )
-            turnAngle = 20;
-        else
-            turnAngle = -20;
-        if (velocity <= .1f && velocity >= -.15f)
-            velocity += Time.deltaTime* .1f * Input.GetAxis("Vertical");
-        else if (velocity > .1f)
-            velocity = .1f;
-        else 
-            velocity = -.15f;
-        Vector3 rot = transform.eulerAngles;
-        transform.rotation = Quaternion.Euler(rot.x,rot.y+turnAngle*Time.deltaTime,rot.z);
-        transform.GetChild(0).localRotation = Quaternion.Euler(0,-turnAngle,0);
-        transform.GetChild(1).localRotation = Quaternion.Euler(0,turnAngle,0);
-        transform.position -= transform.forward * velocity;
-        Vector3 rotorrot = rotor.transform.eulerAngles;
-        rotor.transform.localRotation = Quaternion.Euler(0,0,rotorrot.z+velocity*10);
-        if(Input.GetKeyDown(KeyCode.Space)){
-            if (reloaded && !firing){
-                firing = true;
-                reloaded = false;
+        if (boat == GameManagerASL.playerBoat)
+        {
+            if (turnAngle <= 20 && turnAngle >= -20)
+                turnAngle += Input.GetAxis("Horizontal") * .1f;
+            else if (turnAngle > 20)
+                turnAngle = 20;
+            else
+                turnAngle = -20;
+            if (velocity <= .1f && velocity >= -.15f)
+                velocity += Time.deltaTime * .1f * Input.GetAxis("Vertical");
+            else if (velocity > .1f)
+                velocity = .1f;
+            else
+                velocity = -.15f;
+            Vector3 rot = transform.eulerAngles;
+            transform.rotation = Quaternion.Euler(rot.x, rot.y + turnAngle * Time.deltaTime, rot.z);
+            transform.GetChild(0).localRotation = Quaternion.Euler(0, -turnAngle, 0);
+            transform.GetChild(1).localRotation = Quaternion.Euler(0, turnAngle, 0);
+            transform.position -= transform.forward * velocity;
+            Vector3 rotorrot = rotor.transform.eulerAngles;
+            rotor.transform.localRotation = Quaternion.Euler(0, 0, rotorrot.z + velocity * 10);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (reloaded && !firing)
+                {
+                    firing = true;
+                    reloaded = false;
+                }
             }
+            if (firing && force < 1)
+            {
+                force += 10 * Time.deltaTime;
+            }
+            else if (!firing && !reloaded && force > 0)
+            {
+                force -= .5f * Time.deltaTime;
+            }
+            else if (!firing && !reloaded)
+            {
+                reloaded = true;
+            }
+            else
+            {
+                firing = false;
+            }
+            piston.transform.localPosition = new Vector3(0, -force, 0);
         }
-        if (firing && force < 1){
-            force += 10 * Time.deltaTime;
-        } else if(!firing && !reloaded && force > 0){
-            force -= .5f*Time.deltaTime;
-        } else if (!firing && !reloaded){
-            reloaded = true;
-        } else {
-            firing = false;
-        }
-        piston.transform.localPosition = new Vector3(0,-force,0);
     }
 }
