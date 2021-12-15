effe50934110554950fb8e1ad61f9e48eae63420
nmart510 <nicholas.pa.ma@gmail.com>
2021-12-14 16:42:42 -0800

    Update PistonScript.cs

--- 451 Final/Assets/Source/Model/PistonScript.cs
+++ 451 Final/Assets/Source/Model/PistonScript.cs
@@ -7,7 +7,7 @@ public class PistonScript : MonoBehaviour
     void OnCollisionEnter(Collision col){
         if (col.gameObject.tag == "buoy"){
             if (transform.parent.parent.parent.parent.GetComponent<BoatMovement>().IsFiring())
-                col.rigidbody.AddForce(-transform.up*.02f,ForceMode.Impulse);
+                col.rigidbody.AddForce(-transform.up*.05f,ForceMode.Impulse);
             else
                 col.rigidbody.AddForce(-transform.up*.005f,ForceMode.Impulse);
         }
